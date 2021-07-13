0019ff67dfe0dc9eb491df486be19c15866b1ee9
meron tadesse <merontadesse08@gmail.com>
2021-07-13 09:56:16 -0700

    C - malloc, free

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,58 +1,43 @@
 #include "holberton.h"
-#include <stdlib.h>
-#include <stdio.h>
-int _strLen(char *);
 /**
- * argstostr - takes all args and concatenates them into str
- *
- * @ac: arg count, num of args in av
- * @av: array of pointers to words (args)
- *
- * Return: pointer to beginning of new string
+ * argstostr - prints args
+ * @ac: takes in width of grid
+ * @av: height of grid
+ * Return: the args one line at a time
  */
+
 char *argstostr(int ac, char **av)
 {
-	int i = 0, n = 0, totalSize = 0;
-	char *newStr, *startNewStr;
+	char *str;
+	int count = 0, a = 0, b = 0, c = 0;
 
-	if (ac <= 0 || av == NULL)
+	if (ac == 0 || av == NULL)
 		return (NULL);
-
-	while (i < ac)
+	while (a < ac)
 	{
-		totalSize += _strLen(av[i]) + 1; /* need space for additional new line */
-		i++;
-	}
-	newStr = malloc(sizeof(char) * totalSize + 1); /* plus null byte */
-	if (newStr == NULL) /* ran out of memory */
-		return (NULL);
-	startNewStr = newStr;
-
-	i = 0;
-	while (i < ac)
-	{
-		for (n = 0; av[i][n]; n++)
+		b = 0;
+		while (av[a][b] != '\0')
 		{
-			*newStr++ = av[i][n];
+			count++;
+			b++;
 		}
-		*newStr++ = '\n';
-		i++;
+		a++;
 	}
-	*newStr = '\0';
-	return (startNewStr);
-}
-/**
- * _strLen - returns length of string
- *
- * @s: string to check
- *
- * Return: integer, length of string
- */
-int _strLen(char *s)
-{
-	int strL = 0;
-
-	while (*(s + strL))
-		strL++;
-	return (strL);
+	count = count + ac + 1;
+	str = malloc(sizeof(char) * count);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (a = 0; a < ac; a++)
+	{
+		for (b = 0; av[a][b] != '\0'; b++)
+		{
+			str[c] = av[a][b];
+			c++;
+		}
+		str[c] = '\n';
+		c++;
+	}
+	return (str);
 }
