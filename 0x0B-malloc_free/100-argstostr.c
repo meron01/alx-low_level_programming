44b18f3f36e330b83e54043a14a80df1739c1710
meron tadesse <merontadesse08@gmail.com>
2021-07-13 09:52:17 -0700

    C - malloc, free

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,33 +1,58 @@
 #include "holberton.h"
 #include <stdlib.h>
-
+#include <stdio.h>
+int _strLen(char *);
 /**
- * argstostr - concatenates all arguments to one string
- * @ac: the argument count
- * @av: the argument array
+ * argstostr - takes all args and concatenates them into str
  *
- * Return: pointer to concatenated string
+ * @ac: arg count, num of args in av
+ * @av: array of pointers to words (args)
+ *
+ * Return: pointer to beginning of new string
  */
 char *argstostr(int ac, char **av)
 {
-	int i, j, k = 0, l = 0;
-	char *ret;
+	int i = 0, n = 0, totalSize = 0;
+	char *newStr, *startNewStr;
 
-	if (ac < 1 || !av)
+	if (ac <= 0 || av == NULL)
 		return (NULL);
-	for (i = 0; i < ac; i++)
-		for (j = 0; av[i][j]; j++)
-			l++;
-	l += ac + 1;
-	ret = malloc(sizeof(char) * l);
-	if (!ret)
-		return (NULL);
-	for (i = 0; i < ac; i++)
+
+	while (i < ac)
 	{
-		for (j = 0; av[i][j]; j++)
-			ret[k++] = av[i][j];
-		ret[k++] = '\n';
+		totalSize += _strLen(av[i]) + 1; /* need space for additional new line */
+		i++;
 	}
-	ret[k] = '\0';
-	return (ret);
+	newStr = malloc(sizeof(char) * totalSize + 1); /* plus null byte */
+	if (newStr == NULL) /* ran out of memory */
+		return (NULL);
+	startNewStr = newStr;
+
+	i = 0;
+	while (i < ac)
+	{
+		for (n = 0; av[i][n]; n++)
+		{
+			*newStr++ = av[i][n];
+		}
+		*newStr++ = '\n';
+		i++;
+	}
+	*newStr = '\0';
+	return (startNewStr);
+}
+/**
+ * _strLen - returns length of string
+ *
+ * @s: string to check
+ *
+ * Return: integer, length of string
+ */
+int _strLen(char *s)
+{
+	int strL = 0;
+
+	while (*(s + strL))
+		strL++;
+	return (strL);
 }
